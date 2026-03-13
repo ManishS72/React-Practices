cfc0bc4e981fc0c9d90e066c21a0cae55c5bb1e5
Manish <mkk725050@gmail.com>
2026-03-14 00:01:39 +0530

    hii

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 // import Footer from './Footer';
 import Profile from "./Profile";
+import Profile1 from './Profile1';
 
 function App() {
   return (
     <>
       {/* <Footer/> */}
       <Profile/>
+      <Profile1>
     </>
   );
 }
---
+++ basic-react-app/src/Profile1.jsx
@@ -0,0 +1,11 @@
+
+function Profile1(props)
+{
+    return(
+        <div>
+            <h1>Name : {props.name}</h1>
+            <p>Age : {props.age}</p>
+        </div>
+    );
+}
+export default Profile1;
