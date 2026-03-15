78a689bc8d0ec8c3c5c8ff19d7b1dcf25fff6c1d
Manish <mkk725050@gmail.com>
2026-03-15 23:08:01 +0530

    Add another function

--- basic-react-app/src/Button.jsx
+++ basic-react-app/src/Button.jsx
@@ -4,11 +4,17 @@ function printHello()
     console.log("Hello, Manish Sharma");
 }
 
+function printDemo()
+{
+    console.log("Bye!");
+}
+
 function Button()
 {
     return(
         <div>
             <button onClick={printHello}>Click Me</button>
+            <p onClick={printDemo}>It is a React Event Handling</p>
         </div>
     );
 }
